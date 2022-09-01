84b655d0bfb2fe2dd44755e36d93597195cb1052
Slendermanyshka <slendermanyshka@gmail.com>
2022-09-01 21:06:03 +0300

    hw3

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const Contact = require("./contact.js");
+const statusJoiSchema = require("./contact.js");
+const contactsJoiSchema = require("./contact.js");
+
+module.exports = {
+   Contact,
+   statusJoiSchema,
+   contactsJoiSchema
+}
